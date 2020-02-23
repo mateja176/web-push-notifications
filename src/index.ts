320c0e88aae39f510920b9015aba917670796d1c
mateja <mateja176@gmail.com>
2020-02-23 13:51:43 +0100

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import * as express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => res.json({ hello: 'world' }));
+
+app.listen(3000, () => console.log('http://localhost:3000'));
