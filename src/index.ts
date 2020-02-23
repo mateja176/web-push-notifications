369d67b762bfcd9c5706a4f3e8bcc6e57c5daeab
mateja <mateja176@gmail.com>
2020-02-23 13:56:11 +0100

    feat: adds public and private vapid keys

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import * as express from 'express';
 
+const publicVapidKey =
+  'BKdLrLW2cNG_4_YRGuU-P3DR_hMnYfms1EegXnz5nEhGsC41WryTIxmU0Geix5KNSD05RzpXRRE5JCSnxgsyUMc';
+
+const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
+
+if (!privateVapidKey) {
+  throw new Error('Private VAPID key not supplied');
+}
+
 const app = express();
 
 app.get('/', (req, res) => res.json({ hello: 'world' }));
